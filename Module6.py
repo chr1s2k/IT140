7b422b532edd6293326ad397d32a82a8b818dadf
chr1s2k <barrett.ch@gmail.com>
2018-02-13 10:41:32 -0500

    Mod 6 update

--- Module6.py
+++ Module6.py
@@ -41,3 +41,14 @@ print(data)
 
 
 # Reading a text file
+
+filepath= 'content/textfiles/parrot.txt'
+
+# Open the file in filename for reading
+file1= open(filepath, 'r')
+
+# Read the entire file into the variable
+data = file1.read()
+
+# Print out the contents
+print(data)
